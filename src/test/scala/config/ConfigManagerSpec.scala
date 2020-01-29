e05d9ed8d5a19d506f08bb1edf08e31ea9e5191e
sitetester <sitetester2010@gmail.com>
2020-01-29 23:01:59 +0200

    unit test added

--- src/test/scala/config/ConfigManagerSpec.scala
+++ src/test/scala/config/ConfigManagerSpec.scala
@@ -1,5 +1,11 @@
 package config
 
+import jdbc2scala.config.{AppConfig, ConfigManager}
 import org.scalatest.flatspec.AnyFlatSpec
 
-class ConfigManagerSpec extends AnyFlatSpec {}
+class ConfigManagerSpec extends AnyFlatSpec {
+
+  "it" should "load configuration" in {
+    assert(ConfigManager.loadConfig().isInstanceOf[AppConfig])
+  }
+}
