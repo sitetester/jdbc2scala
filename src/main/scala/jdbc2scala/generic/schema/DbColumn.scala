551d54505e998dbee9f1738e3ebbc54b6e1521f7
sitetester <sitetester2010@gmail.com>
2020-05-12 23:24:57 +0300

    isUnique added

--- src/main/scala/jdbc2scala/generic/schema/DbColumn.scala
+++ src/main/scala/jdbc2scala/generic/schema/DbColumn.scala
@@ -3,5 +3,6 @@ package jdbc2scala.generic.schema
 case class DbColumn(name: String,
                     isPrimary: Boolean = false,
                     isNull: Boolean = false,
+                    isUnique: Boolean = false,
                     dataType: String = "VARCHAR(255)",
 )
