cb708a2971d533b440004bd7150d9b4dfc43a201
sitetester <sitetester2010@gmail.com>
2020-05-13 14:31:09 +0300

    AbstractSchemaBuilder updated

--- src/main/scala/jdbc2scala/drivers/sqlite/schema/AbstractSchemaBuilder.scala
+++ src/main/scala/jdbc2scala/drivers/sqlite/schema/AbstractSchemaBuilder.scala
@@ -1,29 +1,68 @@
 package jdbc2scala.drivers.sqlite.schema
 
-import jdbc2scala.generic.schema.{DbTable, SchemaBuilder}
+import jdbc2scala.generic.schema.{DbColumn, DbTable, SchemaBuilder}
 
 class AbstractSchemaBuilder extends SchemaBuilder {
 
   override def getCreateTableSql(dbTable: DbTable): String = {
 
-    var sql = "\nCREATE TABLE IF NOT EXISTS `" + dbTable.name + "`(\n"
-    println(dbTable.columns)
+    var sql = "\nCREATE TABLE IF NOT EXISTS `" + dbTable.name + "` (\n"
 
-    /*for ((column, index) <- dbTable.columns.zipWithIndex) {
-      val notNull = if (!column.isNull) "NOT NULL" else ""
-      val lastComma = if (!(dbTable.columns.size - index == 1)) {
-        ","
+    var counter = 0
+    val columnsLength = dbTable.columns.length
+    dbTable.columns.foreach(column => {
+      counter += 1
+
+      sql += "`" + getName(column) + "`"
+      sql += getOptions(column)
+
+      if (counter < columnsLength) {
+        sql += ",\n"
       } else {
-        ""
+        sql += "\n)"
       }
-      sql += "\t" + column.name + " " + column.dataType
-        .toUpperCase() + " " + notNull + lastComma + "\n"
+    })
+
+    sql
+  }
+
+  private def getOptions(column: DbColumn): String = {
+
+    var options = ""
+    val cols = column.productElementNames
+    val vals = column.productIterator.toList
+    val zipped = cols.zip(vals)
+
+    val otherOptions = zipped.filterNot(_._1 == "name").toList
+
+    options += " " + getOption("dataType", otherOptions)
+
+    val isNull = getOption("isNull", otherOptions)
+    if (!isNull.asInstanceOf[Boolean]) {
+      options += " NOT NULL"
     }
 
-    sql += ");"
-    sql*/
+    val isUnique = getOption("isUnique", otherOptions)
+    if (isUnique.asInstanceOf[Boolean]) {
+      options += " UNIQUE"
+    }
 
-    ""
+    options += " " + getOption("default", otherOptions)
+
+    options
+  }
+
+  private def getOption(option: String, otherOptions: List[(String, Any)]): Any = {
+    otherOptions.filter(_._1 == option).head._2
+  }
+
+  private def getName(column: DbColumn): String = {
+
+    val cols = column.productElementNames
+    val vals = column.productIterator.toList
+    val zipped = cols.zip(vals)
+
+    zipped.filter(_._1 == "name").toList.head._2.toString
   }
 
   override def getDropTableSql(dbTable: DbTable): String = {
