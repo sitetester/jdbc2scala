4ea473ae042e72e7459e62c59b60d991949fb5c9
sitetester <sitetester2010@gmail.com>
2020-05-13 12:21:00 +0300

    `default` option added

--- src/main/scala/jdbc2scala/generic/schema/DbColumn.scala
+++ src/main/scala/jdbc2scala/generic/schema/DbColumn.scala
@@ -1,8 +1,9 @@
 package jdbc2scala.generic.schema
 
+// e.g. CREATE INDEX "averageRating_idx" on "title_ratings" ("averageRating")
 case class DbColumn(name: String,
                     isPrimary: Boolean = false,
                     isNull: Boolean = false,
                     isUnique: Boolean = false,
                     dataType: String = "VARCHAR(255)",
-)
+                    default: String = "")
