dbcb6511e3f543d7c5a5041cd20273ec7d13dcf7
sitetester <sitetester2010@gmail.com>
2020-05-13 12:21:43 +0300

    DbTable updated with dbIDColumn & indexes

--- src/main/scala/jdbc2scala/generic/schema/DbTable.scala
+++ src/main/scala/jdbc2scala/generic/schema/DbTable.scala
@@ -1,4 +1,8 @@
 package jdbc2scala.generic.schema
 
 // TODO: Write a shell script for schema generation (similar to Symfony entity generator)
-case class DbTable(name: String, columns: List[DbColumn])
+// https://www.sqlitetutorial.net
+case class DbTable(name: String,
+                   columns: List[DbColumn],
+                   dbIDColumn: DbIDColumn = DbIDColumn(),
+                   indexes: Seq[DbIndex] = Seq())
